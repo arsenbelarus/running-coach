8b368ee2035add2cd4f8d8141e980aea81bc7e20
Vaskanian <avaskanian@luxoft.com>
2026-05-13 09:27:07 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+VERIFY_TOKEN = "mytoken123"
+
+@app.get("/")
+def home():
+    return {"message": "Bot alive"}
+
+@app.get("/webhook")
+def verify_webhook(
+    hub_mode: str = "",
+    hub_verify_token: str = "",
+    hub_challenge: str = ""
+):
+    if hub_verify_token == VERIFY_TOKEN:
+        return int(hub_challenge)
+
+    return {"error": "Invalid token"}
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    body = await request.json()
+
+    print(body)
+
+    return {"status": "received"}
