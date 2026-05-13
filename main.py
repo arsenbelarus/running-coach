7b4bb381e90c44d03023fdae473851d5c11c202a
Vaskanian <avaskanian@luxoft.com>
2026-05-13 14:49:41 +0200

    add real env

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, Request
+import requests
+import os
 
 app = FastAPI()
 
 VERIFY_TOKEN = "mytoken123"
+WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 
 @app.get("/")
 def home():
@@ -27,4 +31,31 @@ async def webhook(request: Request):
 
     print(body)
 
+    try:
+        message = body["entry"][0]["changes"][0]["value"]["messages"][0]
+        phone = message["from"]
+        text = message["text"]["body"]
+
+        send_message(phone, f"You said: {text}")
+
+    except Exception as e:
+        print(e)
+
     return {"status": "received"}
+
+
+def send_message(to, text):
+    url = f"https://graph.facebook.com/v22.0/{PHONE_NUMBER_ID}/messages"
+
+    headers = {
+        "Authorization": f"Bearer {WHATSAPP_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "messaging_product": "whatsapp",
+        "to": to,
+        "text": {"body": text}
+    }
+
+    requests.post(url, headers=headers, json=payload)
